796d2f4f75357d0b7bc0acbe774ba158ad3c9ed4
Tasnimul Mohammad Fahim <142605952+HeyFahim@users.noreply.github.com>
2024-01-15 23:12:01 -0800

    Time: 45 ms (76.02%), Space: 17.8 MB (43.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,7 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        r= ''
-        for i in s:
-            if i.isalnum():
-                r+=i.lower()
-        return r==r[::-1]
+        phrase = ''
+        for char in s:
+            if char.isalnum():
+                phrase+=char.lower()
+        return phrase==phrase[::-1]
