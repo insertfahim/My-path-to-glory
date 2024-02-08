1e41aa9f417e0f1b6f8caccf355e4c468ad8e0ed
Tasnimul Mohammad Fahim <142605952+insertfahim@users.noreply.github.com>
2024-02-08 21:29:20 +0600

    Time: 396 ms (11.21%), Space: 16.5 MB (61.93%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        def fibo(i):
+            if i==0:
+                return 0
+            if i==1:
+                return 1
+            return fibo(i-1)+fibo(i-2)
+
+        return fibo(n)
+        
