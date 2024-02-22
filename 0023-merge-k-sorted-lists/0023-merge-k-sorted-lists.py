6be24a0741b3358c3ba0fda4c2ccd010db1d2c67
Tasnimul Mohammad Fahim <142605952+insertfahim@users.noreply.github.com>
2024-02-22 22:49:28 +0600

    Time: 77 ms (68.74%), Space: 19 MB (99.75%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,27 +5,26 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists:
-            return None
-        while len(lists)>1:
-            merged_lists = []
-            for i in range(0,len(lists),2):
-                first_list = lists[i]
-                second_list = lists[i+1] if (i+1)<len(lists) else None
-                merged_lists.append(self.merge(first_list,second_list))
-            lists = merged_lists
-        return lists[0]
-    
-    def merge(self,first_list,second_list):
-        dummy = res = ListNode()
-        while first_list and second_list:
-            if first_list.val<=second_list.val:
-                dummy.next = first_list
+        if len(lists)>0:
+            while len(lists)>1:
+                merged_lists = []
+                for i in range(0,len(lists),2):
+                    list1 = lists[i]
+                    list2 = lists[i+1] if (i+1)<len(lists) else None
+                    merged_lists.append(self.merge(list1, list2))
+                lists = merged_lists
+            return lists[0]
+    def merge(self,list1,list2):
+        dummy= res= ListNode()
+        while list1 and list2:
+            if list1.val<=list2.val:
+                dummy.next=list1
                 dummy=dummy.next
-                first_list=first_list.next
+                list1=list1.next
             else:
-                dummy.next=second_list
+                dummy.next = list2
                 dummy=dummy.next
-                second_list=second_list.next
-        dummy.next = first_list or second_list
+                list2=list2.next
+        dummy.next = list1 or list2
         return res.next
+        
