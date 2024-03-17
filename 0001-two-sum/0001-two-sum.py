9fc9eb8a20d5e34d8d81e8d9936d253dafcaa9de
Tasnimul Mohammad Fahim <faahim06@gmail.com>
2024-03-18 00:12:57 +0600

    Time: 57 ms (74.48%), Space: 17.8 MB (44.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prev_nums = {}
-        for idx in range(len(nums)):
-            gap = target - nums[idx]
-            if gap in prev_nums:
-                return [prev_nums[gap],idx]
+        place_holder = {}
+        for index,value in enumerate(nums):
+            gap = target-value
+            if gap in place_holder:
+                return [place_holder[gap],index]
             else:
-                prev_nums[nums[idx]]=idx
-                
+                place_holder[value]=index
+        
