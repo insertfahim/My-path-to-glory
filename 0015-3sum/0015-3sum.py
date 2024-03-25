cb2ddef80b50d6b8121e3b747bb456cf1b653041
Tasnimul Mohammad Fahim <faahim06@gmail.com>
2024-03-25 16:04:05 +0600

    Time: 492 ms (97.73%), Space: 20.9 MB (25.71%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,5 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        for i,num in enumerate(nums):
+            if num>0:
+                break
+            if i>0 and num==nums[i-1]:
+                continue
+            
+            l,r=i+1,len(nums)-1
+            while l<r:
+                if nums[l]+nums[r]+num>0:
+                    r-=1
+                elif nums[l]+nums[r]+num<0:
+                    l+=1
+                elif nums[l]+nums[r]+num==0:
+                    res.append([nums[r],nums[l],num])
+                    l+=1
+                    r-=1
+                    while nums[l]==nums[i-1] and l<r:
+                        l+=1
+        return res
+    
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
         nums.sort()
 
