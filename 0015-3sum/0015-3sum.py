595d77c34554dea89d0926d5205174e9f560f20b
Tasnimul Mohammad Fahim <faahim06@gmail.com>
2024-03-25 19:04:26 +0600

    Time: 521 ms (94.96%), Space: 20.7 MB (70.32%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,18 +5,19 @@ class Solution:
         for i,num in enumerate(nums):
             if num>0:
                 break
-            if i>0 and num==nums[i-1]:
+            if i>0 and nums[i]==nums[i-1]:
                 continue
             l,r=i+1,len(nums)-1
             while l<r:
-                if nums[l]+nums[r]+num>0:
+                threesum = num+nums[l]+nums[r]
+                if threesum>0:
                     r-=1
-                elif nums[l]+nums[r]+num<0:
+                elif threesum<0:
                     l+=1
-                elif nums[l]+nums[r]+num==0:
-                    res.append([nums[r],nums[l],num])
+                else:
+                    res.append([num,nums[l],nums[r]])
                     l+=1
                     r-=1
-                    while nums[r]==nums[r+1] and r>l:
-                        r-=1
+                    while l<r and nums[l]==nums[l-1]:
+                        l+=1
         return res
