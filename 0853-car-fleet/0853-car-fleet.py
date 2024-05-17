9d06a529b152f3ba635bcd2dfcd0a51fca66876b
Tasnimul Mohammad Fahim <faahim06@gmail.com>
2024-05-17 21:59:35 +0600

    Time: 642 ms (91.92%), Space: 34.2 MB (98.59%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,11 +1,10 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pairs=[(position[i],speed[i]) for i in range(len(position))]
-        fleets = curtime = 0
+        pairs = [(position[i],speed[i]) for i in range(len(position))]
+        fleets = currtime = 0
         for distance,car_speed in sorted(pairs,reverse=True):
             destination_time = (target-distance)/car_speed
-            if curtime<destination_time:
+            if destination_time>currtime:
                 fleets+=1
-                curtime = destination_time
+                currtime=destination_time
         return fleets
-        
