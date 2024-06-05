e6b5c83bc9071b640b9606683107d65b87a0c5bc
Tasnimul Mohammad Fahim <faahim06@gmail.com>
2024-06-05 22:32:32 +0600

    Time: 73 ms (78.76%), Space: 19.3 MB (71.53%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,30 +1,35 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if len(lists)>0:
-            while len(lists)>1:
-                merged_lists = []
-                for i in range(0,len(lists),2):
-                    list1 = lists[i]
-                    list2 = lists[i+1] if (i+1)<len(lists) else None
-                    merged_lists.append(self.merge(list1, list2))
-                lists = merged_lists
-            return lists[0]
+        if not lists or len(lists)==0:
+            return None
+        while len(lists)>1:
+            mergedLists = []
+            for i in range(0,len(lists),2):
+                list1=lists[i]
+                list2=lists[i+1] if i+1<len(lists) else None
+                mergedLists.append(self.merge(list1,list2))
+            lists=mergedLists
+        return lists[0]
+        
     def merge(self,list1,list2):
-        dummy= res= ListNode()
+        head = ListNode(-1)
+        dummy=head
         while list1 and list2:
             if list1.val<=list2.val:
                 dummy.next=list1
-                dummy=dummy.next
                 list1=list1.next
-            else:
-                dummy.next = list2
                 dummy=dummy.next
+            else:
+                dummy.next=list2
                 list2=list2.next
-        dummy.next = list1 or list2
-        return res.next
-        
+                dummy=dummy.next
+        if list1:
+            dummy.next=list1
+        if list2:
+            dummy.next=list2
+        return head.next
