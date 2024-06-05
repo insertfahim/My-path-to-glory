df526e03fd7b80c8b1997549ff764febf20550d5
Tasnimul Mohammad Fahim <faahim06@gmail.com>
2024-06-05 21:38:57 +0600

    Time: 1199 ms (27.54%), Space: 23.8 MB (89.41%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,19 +1,35 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        return self.quickSort(nums)
-
-    def quickSort(self, nums):
-        if len(nums) <= 1:
-            return nums
-
-        pivot = random.choice(nums)
-        lesser, equal, greater = [], [], []
-        for num in nums:
-            if num < pivot:
-                lesser.append(num)
-            elif num == pivot:
-                equal.append(num)
-            else:
-                greater.append(num)
         
-        return self.quickSort(lesser) + equal + self.quickSort(greater)
+        def merge(arr,start,mid,end):
+            first = arr[start:mid+1]
+            second=arr[mid+1:end+1]
+            k=start
+            i,j=0,0
+            while i<len(first) and j<len(second):
+                if first[i]<=second[j]:
+                    arr[k]=first[i]
+                    i+=1
+                    k+=1
+                else:
+                    arr[k]=second[j]
+                    j+=1
+                    k+=1
+            while i<len(first):
+                arr[k]=first[i]
+                i+=1
+                k+=1
+            while j<len(second):
+                arr[k]=second[j]
+                j+=1
+                k+=1
+        
+        def mergesort(arr,start,end):
+            if (end-start)+1<=1:
+                return arr
+            mid = (start+end)//2
+            mergesort(arr,start,mid)
+            mergesort(arr,mid+1,end)
+            merge(arr,start,mid,end)
+            return arr
+        return mergesort(nums,0,len(nums)-1)
